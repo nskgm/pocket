edaca6465efbb9afdf89f37fbedcb431e26ca88f
nskgm <scarlet_sister_remifla@yahoo.co.jp>
2016-07-11 00:12:01 +0900

    多分プリコンパイル対応

---
+++ pocket/pch.h
@@ -0,0 +1,22 @@
+#ifndef __POCKET_PCH_H__
+#define __POCKET_PCH_H__
+
+#include "./math/all.h"
+#include "./math/color.h"
+#include "./math/frustum.h"
+#include "./math/fwd.h"
+#include "./math/line.h"
+#include "./math/math_traits.h"
+#include "./math/matrix3x3.h"
+#include "./math/matrix4x4.h"
+#include "./math/plane.h"
+#include "./math/quaternion.h"
+#include "./math/ray.h"
+#include "./math/rectangle.h"
+#include "./math/simd_traits.h"
+#include "./math/template.h"
+#include "./math/vector2.h"
+#include "./math/vector3.h"
+#include "./math/vector4.h"
+
+#endif // __POCKET_PCH_H__
