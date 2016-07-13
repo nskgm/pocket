591d0d7eade9919e1db10cc79f6d444874ed23a5
nskgm <scarlet_sister_remifla@yahoo.co.jp>
2016-07-13 23:07:52 +0900

    PCH更新

---
+++ pre_compile_headers.h
@@ -0,0 +1,22 @@
+#ifndef __POCKET_PRE_COMPILE_HEADERS_H__
+#define __POCKET_PRE_COMPILE_HEADERS_H__
+
+#include "pocket/math/all.h"
+#include "pocket/math/color.h"
+#include "pocket/math/frustum.h"
+#include "pocket/math/fwd.h"
+#include "pocket/math/line.h"
+#include "pocket/math/math_traits.h"
+#include "pocket/math/matrix3x3.h"
+#include "pocket/math/matrix4x4.h"
+#include "pocket/math/plane.h"
+#include "pocket/math/quaternion.h"
+#include "pocket/math/ray.h"
+#include "pocket/math/rectangle.h"
+#include "pocket/math/simd_traits.h"
+#include "pocket/math/template.h"
+#include "pocket/math/vector2.h"
+#include "pocket/math/vector3.h"
+#include "pocket/math/vector4.h"
+
+#endif // __POCKET_PRE_COMPILE_HEADERS_H__
